77f3b28733fb3bb07e12740847c0b5a15aabf1fa
tania-patsai <tpacaj2301@gmail.com>
2024-09-04 20:38:26 +0300

    updated url

--- weather-app/src/app/shared/constants/config.ts
+++ weather-app/src/app/shared/constants/config.ts
@@ -1,2 +1,2 @@
 export const API_KEY = '5149f6624574fe005b375b92d75e64bb';
-export const API_URL = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org';
+export const API_URL = 'http://api.openweathermap.org';
